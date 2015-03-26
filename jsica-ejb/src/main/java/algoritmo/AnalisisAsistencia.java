3e11e3df210938875066304c4ea92764c2ae845d
Documentos <Documentos@Doc-PC>
2015-03-26 14:44:59 -0500

    Log en Analisis de Asistencia

--- jsica-ejb/src/main/java/algoritmo/AnalisisAsistencia.java
+++ jsica-ejb/src/main/java/algoritmo/AnalisisAsistencia.java
@@ -9,14 +9,6 @@ import com.personal.utiles.FechaUtil;
 import com.project.jsica.ejb.entidades.CambioTurno;
 import com.project.jsica.ejb.entidades.DetalleHorario;
 import com.project.jsica.ejb.entidades.DetalleRegistroAsistencia;
-import dao.DetalleHorarioFacadeLocal;
-import dao.EmpleadoHorarioFacadeLocal;
-import dao.HorarioFacadeLocal;
-import dao.JornadaFacadeLocal;
-import dao.MarcacionFacadeLocal;
-import dao.RegistroAsistenciaFacadeLocal;
-import dao.TCAnalisisFacadeLocal;
-import dao.TCSistemaFacadeLocal;
 import com.project.jsica.ejb.entidades.Empleado;
 import com.project.jsica.ejb.entidades.EmpleadoPermiso;
 import com.project.jsica.ejb.entidades.Feriado;
@@ -27,8 +19,16 @@ import com.project.jsica.ejb.entidades.TCAnalisis;
 import com.project.jsica.ejb.entidades.TCSistema;
 import com.project.jsica.ejb.entidades.Vacacion;
 import dao.CambioTurnoFacadeLocal;
+import dao.DetalleHorarioFacadeLocal;
+import dao.EmpleadoHorarioFacadeLocal;
 import dao.EmpleadoPermisoFacadeLocal;
 import dao.FeriadoFacadeLocal;
+import dao.HorarioFacadeLocal;
+import dao.JornadaFacadeLocal;
+import dao.MarcacionFacadeLocal;
+import dao.RegistroAsistenciaFacadeLocal;
+import dao.TCAnalisisFacadeLocal;
+import dao.TCSistemaFacadeLocal;
 import dao.VacacionFacadeLocal;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -36,6 +36,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
+import org.apache.log4j.Logger;
 import org.jfree.date.DateUtilities;
 
 /**
@@ -344,6 +345,8 @@ public class AnalisisAsistencia {
             return null;
         }
     }
+    
+    private static final Logger LOG = Logger.getLogger(AnalisisAsistencia.class.getName());
 
     private DetalleRegistroAsistencia analizarTurno(Empleado empleado,
             RegistroAsistencia registro,
@@ -371,6 +374,7 @@ public class AnalisisAsistencia {
             long tardanzaEntrada = tardanza(marcacionInicio.getHora(), turnoTolerancia);
 
             detalle.setHoraInicio(marcacionInicio.getHora());
+            LOG.info("Hora Inicio: "+ marcacionInicio.getHora());
             if (tardanzaEntrada > 0) {
                 resultadoInicio = 'T';
             } else {
